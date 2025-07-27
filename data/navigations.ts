c7e88bb4895ccf7662bbed5e8ef89b36c2a73047
Xenvoid <xenvoid404@gmail.com>
2025-07-27 20:54:23 +0000

    penambahan halaman about

--- data/navigations.ts
+++ data/navigations.ts
@@ -11,6 +11,7 @@ interface Navigation {
 export const navigations: Navigation[] = [
     { title: 'Home', href: '/', isExternal: false },
     { title: 'Tools', href: '/tools', isExternal: false },
+    { title: 'About', href: '/about', isExternal: false },
     { title: 'GitHub', href: 'https://github.com/xenvoid404', isExternal: true, icon: FiGithub }
 ];
 
