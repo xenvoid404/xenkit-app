b39e5fb51461444393ce156129fcfb856013cd1b
Xenvoid <xenvoid404@gmail.com>
2025-09-12 03:28:16 +0700

    section categories lupa di import ngentot

--- app/(main)/(home)/page.tsx
+++ app/(main)/(home)/page.tsx
@@ -1,10 +1,12 @@
 import { Hero } from '@/app/(main)/(home)/components/section/hero';
+import { CategoriesSection } from '@/app/(main)/(home)/components/section/categories-section';
 import { FeaturedTools } from '@/app/(main)/(home)/components/section/featured-tools';
 
 export default function Page() {
     return (
         <div id="main-content" className="flex flex-col">
             <Hero />
+            <CategoriesSection />
             <FeaturedTools />
         </div>
     );
