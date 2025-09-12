770631f592e6d23940a0585254071468a95d1205
Xenvoid <xenvoid404@gmail.com>
2025-09-12 19:15:23 +0700

    perbaikan animasi

--- components/motion/animations.tsx
+++ components/motion/animations.tsx
@@ -1,15 +1,26 @@
 'use client';
-import { m, LazyMotion, domAnimation } from 'framer-motion';
+import { m, LazyMotion, AnimatePresence, domAnimation } from 'framer-motion';
 import { slideUpVariants, slideDownVariants } from '@/components/motion/variants';
 import { type ReactNode } from 'react';
 
 export function LazyContainer({ children }: { children: ReactNode }) {
-    return <LazyMotion features={domAnimation}>{children}</LazyMotion>;
+    return (
+        <LazyMotion features={domAnimation}>
+            <AnimatePresence>{children}</AnimatePresence>
+        </LazyMotion>
+    );
 }
 
 export function AnimatedSlideUp({ children, className, ...props }: { children: ReactNode; className?: string }) {
     return (
-        <m.div variants={slideUpVariants} className={className} {...props}>
+        <m.div
+            variants={slideUpVariants}
+            initial="hidden"
+            whileInView="visible"
+            viewport={{ once: true, amount: 0.2 }}
+            className={className}
+            {...props}
+        >
             {children}
         </m.div>
     );
@@ -17,7 +28,14 @@ export function AnimatedSlideUp({ children, className, ...props }: { children: R
 
 export function AnimatedSlideDown({ children, className, ...props }: { children: ReactNode; className?: string }) {
     return (
-        <m.div variants={slideDownVariants} className={className} {...props}>
+        <m.div
+            variants={slideDownVariants}
+            initial="hidden"
+            whileInView="visible"
+            viewport={{ once: true, amount: 0.2 }}
+            className={className}
+            {...props}
+        >
             {children}
         </m.div>
     );
