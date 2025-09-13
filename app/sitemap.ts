6d5380fa6cd23ddc609334cd0748ad594f3216bd
Xenvoid <xenvoid404@gmail.com>
2025-09-14 03:08:11 +0700

    apasih ngentot pake salah path segala

--- app/sitemap.ts
+++ app/sitemap.ts
@@ -1,5 +1,5 @@
 import { MetadataRoute } from 'next';
-import { tools } from '@/app/((main)/(home)/lib/tools-data';
+import { tools } from '@/app/(main)/(home)/lib/tools-data';
 
 export default function sitemap(): MetadataRoute.Sitemap {
     const baseUrl = 'https://xenkit.my.id';
